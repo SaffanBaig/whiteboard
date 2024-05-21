e24e9e567cf12523fc6a606b51c7cf1268ae0e8f
SaffanBaig <mirzasaffan492@gmail.com>
2024-05-21 18:27:52 +0500

    Show current and other users using liveblock

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "#DC2626",
+  "#F59E0B",
+  "#10B981",
+  "#14B8A6",
+  "#6366F1",
+  "#8B5CF6",
+  "#EC4899",
+  "#F472B6",
+]
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length]
 }
